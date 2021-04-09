1c68acd22f463ce477088dddd64b33f2b7e6cd89
kgarc044 <46533611+kgarc044@users.noreply.github.com>
2021-04-09 00:53:40 -0700

    Implemented Game Mechanics
    Can attack w/ Animation
    Can switch to water element only

--- Assets/Scripts/CharacterMechanics.cs
+++ Assets/Scripts/CharacterMechanics.cs
@@ -4,40 +4,79 @@ using UnityEngine;
 
 public class CharacterMechanics : MonoBehaviour
 {
-    public Sprite Fire;
-    public Sprite Electric;
-    public Sprite Water;
-    public Sprite Earth;
+    public GameObject cg;
+    //public RuntimeAnimatorController Fire;
+    //public RuntimeAnimatorController Electric;
+    //public RuntimeAnimatorController Water;
+    //public RuntimeAnimatorController Earth;
+
+    //public RuntimeAnimatorController FireAttack;
+    //public RuntimeAnimatorController ElectricAttack;
+    public RuntimeAnimatorController WaterAttack;
+    //public RuntimeAnimatorController EarthAttack;
+
+
+    string elementCheck;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //cg.gameObject.AddComponent<Animator>();
         ChangeSprite();
+        Attack();
     }
 
+    void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.J) && elementCheck == "Water")
+        {
+            GetComponent<Animator>().SetBool("IsAttack", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("IsAttack", false);
+        }
+        /*
+        if (Input.GetKeyDown(KeyCode.J) && elementCheck == "Fire")
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = FireAttack;
+        }
+        if (Input.GetKeyDown(KeyCode.J) && elementCheck == "Electric")
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = ElectricAttack;
+        }
+        if (Input.GetKeyDown(KeyCode.J) && elementCheck == "Earth")
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = EarthAttack;
+        }*/
+    }
     void ChangeSprite()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = Water;
+            cg.gameObject.GetComponent<Animator>().runtimeAnimatorController = WaterAttack;
+            elementCheck = "Water";
         }
-        if (Input.GetKeyDown(KeyCode.Alpha2))
+        /*if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             this.gameObject.GetComponent<SpriteRenderer>().sprite = Fire;
+            elementCheck = "Fire";
         }
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             this.gameObject.GetComponent<SpriteRenderer>().sprite = Electric;
+            elementCheck = "Electric";
         }
         if (Input.GetKeyDown(KeyCode.Alpha4))
         {
             this.gameObject.GetComponent<SpriteRenderer>().sprite = Earth;
-        }
+            elementCheck = "Earth";
+        }*/
     }
 }
