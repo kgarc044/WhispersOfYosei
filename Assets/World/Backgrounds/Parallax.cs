ac16c21f5d30fa514b3a39bc181010bbe9de932e
Raoul <6421655+Raove@users.noreply.github.com>
2021-04-19 18:41:01 -0700

    Update Parallax.cs

--- Assets/World/Backgrounds/Parallax.cs
+++ Assets/World/Backgrounds/Parallax.cs
@@ -13,7 +13,8 @@ public class Parallax : MonoBehaviour
     Vector2 travel => (Vector2)cam.transform.position - startPosition;
 
     float distanceFromSubject => transform.position.z - subject.position.z;
-    float clippingPlane => (cam.transform.position.z + (distanceFromSubject > 0 ? cam.farClipPlane : cam.nearClipPlane));
+    float clippingPlane => (cam.transform.position.z + 
+        (distanceFromSubject > 0 ? cam.farClipPlane : cam.nearClipPlane));
     float parallaxFactor => Mathf.Abs(distanceFromSubject) / clippingPlane;
 
     public void Start()
